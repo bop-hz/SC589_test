5667b67e72ad387c85749a99777b032949656a13
bop <bo.peng@blueye.no>
2017-04-09 13:55:49 +0200

    initial project for FFT hardware

---
+++ sc589_initial_Core1/src/sc589_initial_Core1.h
@@ -0,0 +1,10 @@
+/*****************************************************************************
+ * sc589_initial_Core1.h
+ *****************************************************************************/
+
+#ifndef __SC589_INITIAL_CORE1_H__
+#define __SC589_INITIAL_CORE1_H__
+
+/* Add your custom header content here */
+
+#endif /* __SC589_INITIAL_CORE1_H__ */
---
+++ sc589_initial_Core2/src/sc589_initial_Core2.h
@@ -0,0 +1,10 @@
+/*****************************************************************************
+ * sc589_initial_Core2.h
+ *****************************************************************************/
+
+#ifndef __SC589_INITIAL_CORE2_H__
+#define __SC589_INITIAL_CORE2_H__
+
+/* Add your custom header content here */
+
+#endif /* __SC589_INITIAL_CORE2_H__ */
